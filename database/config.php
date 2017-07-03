6449e088f91469d44d9bfbe02245072c8b12b177
mimidots <muragemartin22@gmail.com>
2017-07-03 12:21:46 +0300

    Renamed the folder from core to database for consistency with any project. Also separated the database  connection configurations.

---
+++ database/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define( "db_server", "localhost" );// database server
+define( "db_user", "root" );// database user
+define( "db_password", "" );// database password
+define( "db_name", "store" );// database name
