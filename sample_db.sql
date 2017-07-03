91df4af2407fb5287357c732184cae9096a9b15b
mimidots <muragemartin22@gmail.com>
2017-07-03 14:58:54 +0300

    Used standard formatting.

--- sample_db.sql
+++ sample_db.sql
@@ -11,9 +11,9 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -26,20 +26,22 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `hello_world` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `random` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id`     INT(11)      NOT NULL,
+  `name`   VARCHAR(255) NOT NULL,
+  `random` VARCHAR(255) NOT NULL
+)
+  ENGINE = InnoDB
+  DEFAULT CHARSET = latin1;
 
 --
 -- Dumping data for table `hello_world`
 --
 
 INSERT INTO `hello_world` (`id`, `name`, `random`) VALUES
-(1, 'Hello', 'uirdfx'),
-(2, 'World', 'dfjkn'),
-(6, 'Word', 'dfcjkv'),
-(7, 'Olla', 'dsxuifjk');
+  (1, 'Hello', 'uirdfx'),
+  (2, 'World', 'dfjkn'),
+  (6, 'Word', 'dfcjkv'),
+  (7, 'Olla', 'dsxuifjk');
 
 --
 -- Indexes for dumped tables
@@ -59,7 +61,8 @@ ALTER TABLE `hello_world`
 -- AUTO_INCREMENT for table `hello_world`
 --
 ALTER TABLE `hello_world`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `id` INT(11) NOT NULL AUTO_INCREMENT,
+  AUTO_INCREMENT = 8;
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
